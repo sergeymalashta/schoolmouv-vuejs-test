4b77e6594a589646289fab6332d8fc23ceabb141
SMalashta <Sergey.Malashta@noveogroup.com>
2019-10-24 12:09:41 +0700

    add create user

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -2,9 +2,15 @@ import * as types from './mutation-types';
 import service from '../../../services'
 
 export default {
-	createUser: ({ dispatch }) => {
-		// const data = await service.createUser()
-		dispatch('ui/showModal', { type: 'result-modal' }, { root: true });
+	createUser: async ({ dispatch, commit, getters }, formData) => {
+		commit(types.SET_FORM_DATA, formData);
+		try {
+			const data = await service.createUser(getters['formData']);
+			console.info(data);
+			dispatch('ui/showModal', { type: 'result-modal' }, { root: true });
+		} catch (e) {
+			console.error(e.toString());
+		}
 	},
 	sendRequestLoginForm: async ({ commit, dispatch }, { email, password }) => {
 		await dispatch('clearRequestErrors');
--- src/store/modules/auth/getters.js
+++ src/store/modules/auth/getters.js
@@ -1,3 +1,4 @@
 export default {
+	formData: state => state.formData,
 	errors: state => state.errors
 };
